519cea14470409cb83fcf41330ebd583f004dd5f
venkatesh <srivenkatesh17041999@gmail.com>
2020-10-18 20:59:50 +0530

    code to get three inputs

---
+++ acas.sh
@@ -0,0 +1,7 @@
+echo "welcome to arthemetic and sorting programs"
+#! /bin/bash -x
+read -p "enter the first input :" a
+read -p "enter the second input :" b
+read -p "enter the third input :" c
+
+
